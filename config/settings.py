38fec2256ced409dda57222b6ea7ad0b3de86db8
Rikyann270 <125094333+Rikyann270@users.noreply.github.com>
2026-02-19 16:48:46 +0300

    trusted origins_down

--- config/settings.py
+++ config/settings.py
@@ -27,18 +27,25 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = []
+CORS_ALLOW_ALL_ORIGINS = True
 
-    'https://edumerc.up.railway.app',
-    'http://localhost:5174',
+# CSRF_TRUSTED_ORIGINS = [
 
-      ]
+#     'https://edumerc.up.railway.app',
+#     'http://localhost:5174',
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5174',
-    'https://edumerc.up.railway.app',
+#       ]
 
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:5174',
+#     'https://edumerc.up.railway.app',
+
+# ]
+
+
+
+
 
 
 
