1bebd6756dec9d1a72fdfdae446a23c7a933807d
Rikyann270 <125094333+Rikyann270@users.noreply.github.com>
2026-02-25 15:10:16 +0300

    Auth fix 3

--- users/urls.py
+++ users/urls.py
@@ -6,10 +6,10 @@ from rest_framework_simplejwt.views import (
 from .views import RegisterView, ProfileMeView, ProfileUpdateView, GoogleLoginView
 
 urlpatterns = [
-    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/google/', GoogleLoginView.as_view(), name='google_login'),
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('google/', GoogleLoginView.as_view(), name='google_login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('profile/me/', ProfileMeView.as_view(), name='profile_me'),
     path('profile/update/', ProfileUpdateView.as_view(), name='profile_update'),
 ]
